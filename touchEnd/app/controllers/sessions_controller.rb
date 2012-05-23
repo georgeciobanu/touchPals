d552e6b4a570393cacbf68f221a987ebf98f76b2
George Ciobanu <geo.ciobanu@gmail.com>
2012-05-23 13:23:07 -0700

    Send partner name along. Create usernames in seeds.rb

--- touchEnd/app/controllers/sessions_controller.rb
+++ touchEnd/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < Devise::SessionsController
       format.json {
         warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
         render :status => 200, :json => { :session => { :error => "Success", :auth_token => current_user.authentication_token },
-                :user => current_user, :partner => current_user.partner}
+                :user => current_user, :partner_username => current_user.partner.username}
       }
     end
   end
