935f514218c59244f0225e7cc356616b15faa030
George Ciobanu <geo.ciobanu@gmail.com>
2012-05-24 14:58:12 -0700

    Chat message now sent correctly to partner

--- touchEnd/app/controllers/token_authentications_controller.rb
+++ touchEnd/app/controllers/token_authentications_controller.rb
@@ -2,12 +2,14 @@ class TokenAuthenticationsController < ApplicationController
   before_filter :authenticate_user!
 
     def create
+      Rails.logger.info("Authentication Token created")
       @user = current_user
       @user.reset_authentication_token!
       redirect_to edit_user_registration_path(@user)
     end
 
     def destroy
+      Rails.logger.info("Authentication Token destroyed")      
       @user = current_user
       @user.authentication_token = nil
       @user.save
