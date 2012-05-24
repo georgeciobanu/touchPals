f889dcf9b75f96e22ff65118a371f96b14112654
Jonathan Cottrell <jonathan@lindenhoney.com>
2012-05-24 15:20:18 -0700

    CHAT functionality complete

--- test_client.js
+++ test_client.js
@@ -2,7 +2,7 @@ var WebSocket = require('ws');
 var ws = new WebSocket('ws://localhost:8000');
 ws.on('open', function() {
     console.log("connected");
-    ws.send('6FguKGd1kLG1qW35wrsY');
+    ws.send('F3VoTpt2fxCKhyuTxfHy');
 });
 ws.on('message', function(data, flags) {
     // flags.binary will be set if a binary data is received
