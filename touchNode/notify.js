d566feaff8982bd7d2f8de5dce19e03e7c2b75fd
George Ciobanu <geo.ciobanu@gmail.com>
2012-05-23 23:21:34 -0700

    The server now sends the message all the way to the client: iPhone -> Rails -> Redis -> WebSocketServer -> iPhone2

--- touchNode/notify.js
+++ touchNode/notify.js
@@ -1,24 +1,36 @@
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({port: 8000});
+var socketByToken = {};
+
 var redis = require("redis"),
-       redisClient = redis.createClient(),
-       msg_count = 0;
+       redisClient = redis.createClient();
 
-	var WebSocketServer = require('ws').Server
-	  , wss = new WebSocketServer({port: 8000});
-	
-	wss.on('connection', function(ws) {
-	    ws.on('message', function(message) {
-	        console.log('received: %s', message);
-	    });
-		console.log("Someone connected:" + ws);
-	    ws.send('something back');
-	});
+wss.on('connection', function(ws) {
 
-  redisClient.on("message", function (channel, message) {
-     console.log("redisClient chat channel: "  + message);
-     msg_count += 1;
-  });
+    console.log("Server: someone connected to me");
+    // console.log('additional info:');
+    // console.log(ws);
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        socketByToken[message] = this;
+        console.log("Size of hash: " + socketByToken.length);
+    });
+});
+
+redisClient.on("message", function (channel, message) {
+   console.log("redisClient chat channel: "  + message);
+   console.log("Size of hash: " + socketByToken.length);
+   for (var k in socketByToken) {
+     console.log("Key: " + k + " Value: " + socketByToken[k]);
+   }
+   socketByToken[message].send("You have mail!");
+});
 
    
   // app.listen(8000);
-  redisClient.subscribe("chats");
-console.log("Started...");
+redisClient.subscribe("chats");
+console.log("Connected to Redis...");
+
+wss.on('close', function(ws) {
+    console.log("Server: someone disconnected to me");
+});
