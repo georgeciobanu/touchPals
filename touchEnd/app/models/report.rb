c198f02f485ec337ddfec1f2200b8601927843df
George Ciobanu <geo.ciobanu@gmail.com>
2012-06-01 20:15:07 -0700

    Enable reportee field in ORM

--- touchEnd/app/models/report.rb
+++ touchEnd/app/models/report.rb
@@ -1,4 +1,5 @@
 class Report < ActiveRecord::Base
   attr_accessible :reportee_id, :reporter_id
   belongs_to :reporter, :class_name => 'User', :foreign_key => :reporter_id
+  belongs_to :reportee, :class_name => 'User', :foreign_key => :reportee_id
 end
