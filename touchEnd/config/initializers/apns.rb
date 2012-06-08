7366b6a459e7c8e1bc5e98ce65f4b0af0f70e493
George Ciobanu <geo.ciobanu@gmail.com>
2012-06-08 14:49:15 -0700

    Push notifications, in production and dev

--- touchEnd/config/initializers/apns.rb
+++ touchEnd/config/initializers/apns.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
-#  APNS.host = 'gateway.push.apple.com' 
+  APNS.host = 'gateway.push.apple.com' 
   APNS.pem  = 'push_chataffair_prod.pem'
-  # gateway.sandbox.push.apple.com is default
 else
+  # gateway.sandbox.push.apple.com is default  
   APNS.pem  = 'push_chataffair_dev.pem'
 end
-# this is the file you just created
+
 
 # APNS.port = 2195
 # this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
